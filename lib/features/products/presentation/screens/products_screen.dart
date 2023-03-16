18c29e80865759167991531a508df8d8d95aef1b
Fernando Herrera <fernando.herrera85@gmail.com>
2023-03-16 12:40:11 -0400

    sidemenu agregado

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:teslo_shop/features/shared/shared.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
+      drawer: SideMenu( scaffoldKey: scaffoldKey ),
       appBar: AppBar(
         title: const Text('Products'),
         actions: [
